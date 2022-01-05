15c33b88781a57972695b121b19cdc0715334171
Ciro Molina <cmolina@itgtec.com>
2022-01-04 22:26:00 -0500

    Largest palidromic number: resolved

---
+++ project-euler/4-largest-palindromic-number.py
@@ -0,0 +1,47 @@
+"""
+Problem 4
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+
+def isPalindrome(arg):
+    """check if a number is palindrome"""
+    return str(arg) == str(arg)[::-1]
+
+
+def getTwoDigitPalindromicMultiples():
+    palindromicNumbers = []
+
+    for i in range(99, 10, -1):
+        for j in range(99, 10, -1):
+            if isPalindrome(i * j):
+                palindromicNumbers.append((f"{i}*{j}", (i * j)))
+
+    return palindromicNumbers
+
+
+def getThreeDigitPalindromicMultiples():
+    palindromicNumbers = []
+
+    for i in range(999, 100, -1):
+        for j in range(999, 100, -1):
+            for k in range(999, 100, -1):
+                if isPalindrome(i * j * k):
+                    palindromicNumbers.append((f"{i}*{j}*{k}", (i * j * k)))
+
+    return palindromicNumbers
+
+
+if __name__ == '__main__':
+    import time
+
+    start = time.time()
+
+    # print(max(getTwoDigitPalindromicMultiples()))
+    print(max(getThreeDigitPalindromicMultiples()))
+
+    end = time.time()
+    print(f"{end - start} seconds")
